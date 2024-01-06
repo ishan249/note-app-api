33743e093665f4061ed254a6ee9e256893655901
ishan249 <249patelishan@gmail.com>
2024-01-06 14:03:34 +0530

    edit note api changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -83,4 +83,37 @@ router.post("/api/addnote", async (req, res) => {
   }
 });
 
+router.put('/api/editnote', async (req, res) => {
+  try {
+    const { username, noteId, heading, notetext, completed } = req.body;
+
+    if (!username || !noteId || !heading || !notetext || completed === undefined) {
+      return res.status(400).json({ error: 'User ID, note ID, heading, notetext, and completed are required.' });
+    }
+
+    const user = await User.findOne({username});
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    const noteIndex = user.notes.findIndex((note) => note._id.toString() === noteId);
+
+    if (noteIndex === -1) {
+      return res.status(404).json({ error: 'Note not found.' });
+    }
+
+    user.notes[noteIndex].heading = heading;
+    user.notes[noteIndex].notetext = notetext;
+    user.notes[noteIndex].completed = completed;
+
+    await user.save();
+
+    res.status(200).json({message:"note saved"});
+  } catch (error) {
+    console.error('Error editing note:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 module.exports = router;
